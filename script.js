c3696535916abc4c3c160994ef97d80994527180
aryaadiwww <159741263+aryaadiwww@users.noreply.github.com>
2024-10-28 11:23:47 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const cube = document.querySelector(".cube");
+    let rotateX = 0;
+    let rotateY = 0;
+
+    setInterval(() => {
+        rotateX += 1;
+        rotateY += 1;
+        cube.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    }, 30);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cube2 = document.querySelector(".cube2");
+    let rotateX = 0;
+    let rotateY = 0;
+
+    setInterval(() => {
+        rotateX += 1;
+        rotateY += 1;
+        cube2.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    }, 30);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cube3 = document.querySelector(".cube3");
+    let rotateX = 0;
+    let rotateY = 0;
+
+    setInterval(() => {
+        rotateX += 1;
+        rotateY += 1;
+        cube3.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    }, 30);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cube4 = document.querySelector(".cube4");
+    let rotateX = 0;
+    let rotateY = 0;
+
+    setInterval(() => {
+        rotateX += 1;
+        rotateY += 1;
+        cube4.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    }, 30);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cube5 = document.querySelector(".cube5");
+    let rotateX = 0;
+    let rotateY = 0;
+
+    setInterval(() => {
+        rotateX += 1;
+        rotateY += 1;
+        cube5.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+    }, 30);
+});
+
